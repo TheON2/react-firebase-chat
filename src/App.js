49929e19a2ce7c6886cf2efcded948c1f1de4bf7
TheON2 <sniperad@naver.com>
2023-01-03 22:54:14 +0900

    Update : App.js Matching RoutePage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, Router, Switch} from "react-router-dom";
+import LoginPage from "./components/LoginPage/LoginPage";
+import ChatPage from "./components/ChatPage/ChatPage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ChatPage}/>
+        <Route exact path="/" component={LoginPage}/>
+        <Route exact path="/" component={RegisterPage}/>
+      </Switch>
+    </Router>
   );
 }
 
