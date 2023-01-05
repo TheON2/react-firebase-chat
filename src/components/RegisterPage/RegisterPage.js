1cf0097be9861876bb44e3a77f0950505f49df33
TheON2 <sniperad@naver.com>
2023-01-05 10:40:01 +0900

    Update : RegisterPage apply Route

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,13 +1,43 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function RegisterPage() {
   return (
-    <div>
-      adfasfasdf
-      <form>
-        <input defaultValue="test"/>
-        <input/>
-        <input type="submit"/>
+    <div className="auth-wrapper">
+      <div style={{textAlign : 'center'}}>
+        <h3>Register</h3>
+      </div>
+      <form
+
+      >
+        {/* register your input into the hook by invoking the "register" function */}
+        <label>Email</label>
+        <input name="email" type="email"
+
+        />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <label>Name</label>
+        <input name="name"
+
+        />
+        {/* errors will return when field validation fails  */}
+        <label>Password</label>
+        <input name="password" type="password"
+
+        />
+        {/* errors will return when field validation fails  */}
+        <label>Password Confirm</label>
+        <input name="password_confirm" type="password"
+
+        />
+        {/* errors will return when field validation fails  */}
+
+
+        <input type="submit" />
+        <Link style={{ color : 'gray' , textDecoration: 'none'}} to="/login">
+          이미 아이디가 있다면 ...
+        </Link>
       </form>
     </div>
   )
